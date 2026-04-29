9acb3eb98cf443934ff9163c8e4109a1e67624ec
jansij750-dot <jansij750@gmail.com>
2026-04-29 13:30:21 +0530

    3rd commit

--- vcubejava/workspace/corejava/JavaInto/src/com/javaIntro/Student.java
+++ vcubejava/workspace/corejava/JavaInto/src/com/javaIntro/Student.java
@@ -3,6 +3,11 @@ package com.javaIntro;
 public class Student {
 	public static void main(String[] args) {
 		System.out.println("java simple");
+		System.out.println("java simple");
+		System.out.println("java simple");
+		System.out.println("java simple");
+		System.out.println("java simple");
+		
 	}
 
 }
